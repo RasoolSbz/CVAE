9f8ac583d8acd75ba5b14a60645c27e7e2a59bb7
s2kamyab <37024590+s2kamyab@users.noreply.github.com>
2018-09-02 20:42:16 +0430

    put voxnet as candidate for encoder

--- _scripts/matlab/visualize_voxel.m
+++ _scripts/matlab/visualize_voxel.m
@@ -21,7 +21,8 @@ p = patch(isosurface(voxel,0.0005));
 set(p,'FaceColor',f_color,'EdgeColor','none');
 daspect([1,1,1])
 %%daspect(ratio) sets the data aspect ratio for the current axes. The data aspect ratio is the relative length of the data units along the x-axis, y-axis, and z-axis. Specify ratio as a three-element vector of positive values that represent the relative lengths of data units along each axis. For example, [1 2 3] indicates that the length from 0 to 1 along the x-axis is equal to the length from 0 to 2 along the y-axis and 0 to 3 along the z-axis. For equal data unit lengths in all directions, use [1 1 1].
-view(3); axis tight
+view(3); 
+axis tight
 camlight
 lighting gouraud;
 axis off;
