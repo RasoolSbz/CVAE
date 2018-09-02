2cdd132544909a97c18a13f7640d22a11dc71ad8
s2kamyab <37024590+s2kamyab@users.noreply.github.com>
2018-09-02 19:53:54 +0430

    sch

--- _scripts/matlab/visualize_voxel.m
+++ _scripts/matlab/visualize_voxel.m
@@ -1,4 +1,4 @@
-function out = visualize(voxel)
+function out = visualize_voxel(voxel)
 %face colors:
 % % 'red' or 'r'	Red	[1 0 0]
 % % 'green' or 'g'	Green	[0 1 0]
@@ -8,12 +8,19 @@ function out = visualize(voxel)
 % % 'cyan' or 'c'	Cyan	[0 1 1]
 % % 'white' or 'w'	White	[1 1 1]
 % % 'black' or 'k
-set(fig,'Color','white', 'Visible', 'off');
+if size(voxel , 2) < 100
+    f_color = 'red';
+else
+    f_color = 'cyan';
+end
+fig_handle = figure();
+set(fig_handle,'Color','white', 'Visible', 'on');
 set(gca,'position',[0,0,1,1],'units','normalized');
 %     voxel = squeeze(voxels(i,:,:,:) > threshold);
-p = patch(isosurface(voxel,0.05));
-set(p,'FaceColor','y','EdgeColor','none');
+p = patch(isosurface(voxel,0.0005));
+set(p,'FaceColor',f_color,'EdgeColor','none');
 daspect([1,1,1])
+%%daspect(ratio) sets the data aspect ratio for the current axes. The data aspect ratio is the relative length of the data units along the x-axis, y-axis, and z-axis. Specify ratio as a three-element vector of positive values that represent the relative lengths of data units along each axis. For example, [1 2 3] indicates that the length from 0 to 1 along the x-axis is equal to the length from 0 to 2 along the y-axis and 0 to 3 along the z-axis. For equal data unit lengths in all directions, use [1 1 1].
 view(3); axis tight
 camlight
 lighting gouraud;
