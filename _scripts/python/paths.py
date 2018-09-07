478c29fcafe4f5eebd3bc6677a9f9ef3b3d687dd
s2kamyab <37024590+s2kamyab@users.noreply.github.com>
2018-09-07 06:38:08 +0430

    add codes for training

---
+++ _scripts/python/paths.py
@@ -0,0 +1,20 @@
+import os
+import sys
+import glob
+
+
+def getDatasetDirectory(username):
+    if username == 'kamyab':
+        path = 'E:/thesis_phd/msSabzi/ModelNet10'
+    elif username == 'sabzi':
+       #TO DO
+    return path
+
+
+
+def getModelWeightsDirectory(username):
+    if username == 'kamyab':
+        path = 'E:/thesis_phd/msSabzi/CVAE_inverse_rendering/saved_models'
+    elif username == 'sabzi':
+       #TO DO
+    return path
---
+++ _scripts/python/training_cvae.py
@@ -0,0 +1,28 @@
+import nn_core
+import gpu
+gpu.setGPU(0)
+
+dataset = 'Modelnet10'
+network_type = 'mrSabzi'
+image_size = 32
+load_saved_model = False
+number_of_epochs = 10
+batch_size = 10
+dropout_ratio = 0.2
+learning_rate = 0.0001
+net_optimizer = 'SGD' # ADAM, SGD, RMSprop, Adagrad, Adadelta, Adamax, Nadam
+loss_fcn = 'categorical_crossentropy' #'weighted_ours'#'categorical_crossentropy' # 'categorical_hinge' # 'categorical_crossentropy' 'mean_squared_error' mean_absolute_error
+username = 'kamyab'
+
+nn_core.trainAndEvaluate( \
+username, \
+dataset, \
+network_type, \
+image_size, \
+load_saved_model, \
+number_of_epochs, \
+batch_size, \
+dropout_ratio, \
+learning_rate, \
+net_optimizer, \
+loss_fcn)
